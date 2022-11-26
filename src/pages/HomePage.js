f227f6deda11b6965c71f9f9af07fd7e1d96fe3e
12mehul <mehul091298@gmail.com>
2022-11-26 22:29:27 +0530

    start

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+        <h1>Welcome to my Blog</h1>
+        <p>This is my Blog</p>
+        </div>
+  );
+}
+
+export default HomePage;
